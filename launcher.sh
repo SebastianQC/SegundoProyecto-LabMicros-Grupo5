7bfa58346f952b27dea782a2dc9e58ce73c0c532
Sebas Quesada <s_quesada96@hotmail.com>
2017-11-23 17:10:57 -0600

    Ejecutable Final
    Archivo encargado de ejecutar en paralelo ambos archivos del proyecto.

---
+++ launcher.sh
@@ -0,0 +1,8 @@
+#1/bin/sh
+# launcher.sh
+# navigate to home directory,then to this directory, then execute python script$
+
+cd /
+cd home/pi/ProyectoG5/
+sudo python sw401.py & sudo python intento.py
+cd /
